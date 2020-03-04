f686c4d81fbb6d94e33f8aa6dd44595947cb0b76
Myumi K <myumiak@gmail.com>
2020-03-04 18:39:42 -0500

    why did i name all my branches different variations of qrcode

--- src/components/FileLoader.vue
+++ src/components/FileLoader.vue
@@ -49,35 +49,37 @@ export default {
         const hints = new Map();
         hints.set(DecodeHintType.TRY_HARDER, true);
         const r = await codeReader.decodeFromImageUrl(iUrl, hints);
-        if (r.resultMetadata.has(ResultMetadataType.STRUCTURED_APPEND_SEQUENCE)
-            && r.resultMetadata.has(ResultMetadataType.BYTE_SEGMENTS)){
-          let sequence_info = r.resultMetadata.get(ResultMetadataType.STRUCTURED_APPEND_SEQUENCE);
-          if ((sequence_info & 0x0F) != 3) {
-            logger.info('Multipart code is not 4 parts.');
+        r.forEach(code => {
+          if (code.resultMetadata.has(ResultMetadataType.STRUCTURED_APPEND_SEQUENCE)
+            && code.resultMetadata.has(ResultMetadataType.BYTE_SEGMENTS)){
+            let sequence_info = code.resultMetadata.get(ResultMetadataType.STRUCTURED_APPEND_SEQUENCE);
+            if ((sequence_info & 0x0F) != 3) {
+              logger.info('Multipart code is not 4 parts.');
+              return;
+            }
+            if (this.currParity != code.resultMetadata.get(ResultMetadataType.STRUCTURED_APPEND_PARITY)) {
+              logger.info(`Resetting parser: new multipart parity number ${this.currParity} -> ${code.resultMetadata.get(10)}`);
+              this.currParity = code.resultMetadata.get(ResultMetadataType.STRUCTURED_APPEND_PARITY);
+              this.currRead = [false, false, false, false];
+            }
+            let currNum = (sequence_info >> 4);
+            logger.info("Multipart code #"+currNum);
+            if (!this.currRead[currNum]) {
+              const inArr = code.resultMetadata.get(ResultMetadataType.BYTE_SEGMENTS)[0];
+              const offset = currNum * 540;
+              for (let i = 0; i < 540 && i < inArr.byteLength; ++i) {
+                this.currDataBuffer[i + offset] = inArr[i];
+              }
+              this.currRead[currNum] = true;
+            }
+            if (this.currRead[0]) {
+              // && this.currRead[1] && this.currRead[2] && this.currRead[3]
+              this.$emit('qr-load', this.currDataBuffer);
+            }
             return;
           }
-          if (this.currParity != r.resultMetadata.get(ResultMetadataType.STRUCTURED_APPEND_PARITY)) {
-            logger.info("Resetting parser: new multipart parity number "+this.currParity+" -> "+r.resultMetadata.get(10));
-            this.currParity = r.resultMetadata.get(ResultMetadataType.STRUCTURED_APPEND_PARITY);
-            this.currRead = [false, false, false, false];
-          }
-          let currNum = (sequence_info >> 4);
-          logger.info("Multipart code #"+currNum);
-          if (!this.currRead[currNum]) {
-            const inArr = r.resultMetadata.get(ResultMetadataType.BYTE_SEGMENTS)[0];
-            const offset = currNum * 540;
-            for (let i = 0; i < 540 && i < inArr.byteLength; ++i) {
-              this.currDataBuffer[i + offset] = inArr[i];
-            }
-            this.currRead[currNum] = true;
-          }
-          if (this.currRead[0]) {
-            // && this.currRead[1] && this.currRead[2] && this.currRead[3]
-            this.$emit('qr-load', this.currDataBuffer);
-          }
-          return;
-        }
-        this.$emit('qr-load', r.resultMetadata.get(2)[0]) // data bytes
+          this.$emit('qr-load', r.resultMetadata.get(2)[0]) // data bytes
+        })
       }
       catch (err) {
         throw err; // throw instead of log, get warning
