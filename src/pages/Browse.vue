d5b203711e46e34333c782ad1b00f5678f9e315c
DamSenViet <26016342+DamSenViet@users.noreply.github.com>
2020-04-07 14:59:52 -0700

    Fixed browse page errors

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -19,7 +19,7 @@
         <h1>Designs</h1>
         <span>{{results.length}} results</span>
         <div id="pattern-list">
-          <a class="pattern-container" v-for="opt in results" :key="opt.bytes" :href="getUrl(opt.bytes)">
+          <a class="pattern-container" v-for="opt in results" :key="opt.bytes" :href="opt.url">
             <h3>{{opt.title}}</h3>
             <div class="type-tags">
               <span v-if="opt.f_type != null" class="tag type">
@@ -142,6 +142,7 @@ export default {
     // map using store module search
     ...mapState('browse', [
       'query',
+      'results',
       'pageNumber'
     ]),
     ...mapGetters('browse', [
