f280b96374f6e9b77662f29ebba6def541ac4e89
Adam Brin <ABrin@getty.edu>
2020-04-10 11:15:02 -0700

    show thumbnail, emit event

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -7,9 +7,16 @@
       Search
     </button>
     <ol>
-      <li v-for="match of matches">
+      <li v-for="match of matches" @click="choose(match)">
         {{ match.title }}
         {{ match.id }}
+        <img
+          :src="
+            'https://media.getty.edu/iiif/image/' +
+              match.manifest +
+              '/full/!150,150/0/default.jpg'
+          "
+        />
       </li>
     </ol>
   </div>
@@ -26,18 +33,26 @@ export default {
     };
   },
   methods: {
+    choose(match) {
+      match.url =
+        "https://media.getty.edu/iiif/image/" +
+        match.manifest +
+        "/full/!300,300/0/default.jpg";
+      console.log("emitting,", match);
+      this.$emit("input", match);
+    },
     search() {
       let query = this.value;
       this.matches = [];
       console.log("searching imageData...", query);
       for (let _line of imageData.split("\n")) {
+        let line = _line.split("|");
         let _query = query.toUpperCase();
-        let _upper = _line.toUpperCase();
+        let _upper = line[0].toUpperCase();
         if (
           (_upper.includes && _upper.includes(_query)) ||
           _upper.indexOf(_query) > -1
         ) {
-          let line = _line.split("|");
           this.matches.push({
             title: line[0],
             id: line[1],
