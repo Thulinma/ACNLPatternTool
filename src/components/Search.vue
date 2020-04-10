e912b46fcf32b43aac7c9efef489b36ebf2b8c89
David Newbury <david.newbury@gmail.com>
2020-04-10 12:04:18 -0700

    trying to improve search performance.
    I should probably profile this.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -8,65 +8,54 @@
     </button>
     <ol>
       <li v-for="match of matches" @click="choose(match)">
-        {{ match.title }}
-        {{ match.id }}
-        <img
-          :src="
-            'https://media.getty.edu/iiif/image/' +
-              match.manifest +
-              '/full/!150,150/0/default.jpg'
-          "
-        />
+        {{ match.full_name }}
+        <a :src="match.webpage">view in collection</a>
+        <img :src="match.iiif_url" />
       </li>
     </ol>
   </div>
 </template>
 <script>
-import imageData from "../data/NoC-US.txt"; // OR: const helloText = require('./hello.txt')
+import imageData from "../data/NoC-US.txt";
+import { extractData } from "../libs/ExtractData.js";
 
 export default {
   name: "Search",
   data() {
     return {
       value: "",
-      matches: []
+      lookupData: [],
+      searchData: [],
+      matches: [],
     };
+  },
+  mounted() {
+    this.searchData = imageData
+      .split("\n")
+      .map((line) => line.split("|")[0].toUpperCase());
+    this.lookupData = imageData.split("\n").map((line) => extractData(line));
   },
   methods: {
     choose(match) {
-      match.url =
-        "https://media.getty.edu/iiif/image/" +
-        match.manifest +
-        "/full/!300,300/0/default.jpg";
-      console.log("emitting,", match);
       this.$emit("input", match);
     },
     search() {
       let query = this.value;
       this.matches = [];
       console.log("searching imageData...", query);
-      for (let _line of imageData.split("\n")) {
-        let line = _line.split("|");
+      this.searchData.forEach((_upper, index) => {
         let _query = query.toUpperCase();
-        let _upper = line[0].toUpperCase();
-        if (
-          (_upper.includes && _upper.includes(_query)) ||
-          _upper.indexOf(_query) > -1
-        ) {
-          this.matches.push({
-            title: line[0],
-            id: line[1],
-            manifest: line[2]
-          });
+        if (_upper.indexOf(_query) > -1) {
+          this.matches.push(this.lookupData[index]);
         }
-      }
-    }
-  }
+      });
+    },
+  },
 };
 </script>
 <style type="text/css" scoped>
 ol {
-  max-height: 10em;
+  max-height: 40em;
   overflow-y: scroll;
 }
 </style>
