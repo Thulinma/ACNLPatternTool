4c4ddcec56135e5bd7e0804aaf06eaa792180056
DamSenViet <26016342+DamSenViet@users.noreply.github.com>
2020-10-17 05:40:56 -0700

    More responsive about page

--- src/components/partials/Passport.vue
+++ src/components/partials/Passport.vue
@@ -1,156 +1,269 @@
 <template>
   <div class="passport">
     <section class="passport-photo">
-      <div class="photo">
-        <img>
+      <div class="photo-frame">
+        <div class="photo-container">
+          <img class="photo" src="" alt="" />
+        </div>
       </div>
-    </section><!-- photo and roles -->
+    </section>
+    <!-- photo and roles -->
 
     <section class="passport-info">
-      <div class="greeting">{{normalizedMember.greeting}}</div><!-- greeting -->
+      <div class="greeting">{{ normalizedMember.greeting }}</div>
+      <!-- greeting -->
       <ul>
         <li class="island">
-          <IconBase 
+          <IconBase
+            class="icon-island"
             icon-name="island"
             icon-color="#4D993E"
-            width="15"
-            height="15"
+            width="20"
+            height="20"
           >
             <IconHouse />
           </IconBase>
-          {{normalizedMember.island}}
+          <span>{{ normalizedMember.island }}</span>
         </li>
-        <li class="title">{{normalizedMember.title}}</li>
-        <li class="name">{{normalizedMember.name}}</li>
+        <li class="title">
+          <span>{{ normalizedMember.title }}</span>
+        </li>
+        <li class="name">
+          <span>{{ normalizedMember.name }}</span>
+        </li>
         <li class="urls">
-          <IconBase 
+          <IconBase
+            class="icon-contact-card"
             icon-name="contact card"
             icon-color="#E976DD"
-            width="15"
-            height="15"
+            width="25"
+            height="25"
           >
             <IconContactCard />
           </IconBase>
-          <a 
-            v-for="link in normalizedMember.urls" 
+          <a
+            v-for="link in normalizedMember.urls"
             :key="normalizedMember.urls.indexOf(link)"
             :href="link.url"
-          >{{link.name}}</a>
+            >{{ link.name }}</a
+          >
         </li>
-      </ul><!-- details -->
-    </section><!-- info -->
-  </div><!-- passport card container -->
+      </ul>
+      <!-- details -->
+    </section>
+    <!-- info -->
+  </div>
+  <!-- passport card container -->
 </template>
 
 <script>
-import IconBase from '/components/icons/IconBase.vue';
-import IconHouse from '/components/icons/IconHouse.vue';
-import IconContactCard from '/components/icons/IconContactCard.vue';
+import IconBase from "/components/icons/IconBase.vue";
+import IconHouse from "/components/icons/IconHouse.vue";
+import IconContactCard from "/components/icons/IconContactCard.vue";
 
 export default {
   props: {
     member: {
       type: Object,
-    }
+    },
   },
   // set defaults if attributes aren't set
   computed: {
     normalizedMember() {
-    	return Object.assign({
-        name: 'Tom Nook',
-        greeting: 'Yes, yes!',
-        title: 'Aspiring Millionarie',
-        island: 'Nintentown',
-        urls: [],
-        roles: [],
-			}, this.member);
-    }
+      return Object.assign(
+        {
+          name: "Tom Nook",
+          greeting: "Yes, yes!",
+          title: "Aspiring Millionarie",
+          island: "Nintentown",
+          urls: [],
+          roles: [],
+        },
+        this.member
+      );
+    },
   },
   components: {
     IconBase,
     IconHouse,
     IconContactCard,
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  $base-font-size: 18px;
-  $cream: #F8F6E5;
-  $pastel-pink: #F3E6DB;
-  $mint-green: #E8F4D4;
-  $white: #FFFFFF;
+@import "styles/colors";
+@import "styles/positioning";
+@import "styles/screens";
 
-  .passport {
-    background-color: $pastel-pink;
-    // background-image: url('/assets/images/leaf-brush-pattern.svg');
-    border-radius: 5px;
-    display: flex;
-    height: 240px;
-    margin: 30px auto;
-    max-width: 650px;
-    padding: 20px 100px;
+.passport {
+  @include relative-in-place;
+  justify-self: stretch;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-columns: 1fr;
+  grid-auto-rows: auto;
+  grid-template-rows: auto;
+  row-gap: 30px;
+  padding: 30px 25px 50px 25px;
+
+  justify-content: center;
+  justify-items: flex-start;
+
+  border-radius: 8px;
+  background-color: $soapstone;
+
+  & ~ & {
+    margin-top: 40px;
   }
 
-  .passport-photo {
-    align-self: center;
-    width: 34%;
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.15;
+    background-image: url("../../assets/images/Four_Leaves_Pattern_BW.svg");
+    background-size: 120px;
+    background-repeat: repeat;
+  }
 
-    .photo {
-      background-color: $cream;
-      border-radius: 14px;
-      height: 180px;
-      width: 180px;
+  @include tablet-portrait {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-auto-columns: auto 1fr;
+    grid-auto-rows: auto;
+    grid-template-rows: auto;
+    align-content: center;
+    align-items: center;
+    row-gap: 0px;
+    column-gap: 50px;
+    padding: 30px 50px 50px 50px;
+  }
+  
+  @include tablet-landscape {
+    column-gap: 80px;
+    padding: 30px 80px 50px 80px;
+    max-width: 950px;
+    justify-self: center;
+  }
+  
+  @include desktop {
+    max-width: 1150px;
+    column-gap: 100px;
+    padding: 50px 100px 80px 100px;
+  }
+}
 
-      display: flex;
-      align-items: center;
-      justify-content: center;
+.passport-photo {
+  @include relative-in-place;
+  justify-self: center;
 
-      img {
-        background-color: $mint-green;
-        border-radius: 14px;
-        padding: 5px;
+  .photo-frame {
+    border-radius: 14px;
+    padding: 10px;
+    background-color: $ecru-white;
+
+    @include desktop {
+      border-radius: 18px;
+      padding: 13px;
+    }
+    
+    .photo-container {
+      background-color: $chrome-white;
+      border-radius: 8px;
+      @include desktop {
+        border-radius: 12px;
+      }
+
+      .photo {
         height: 150px;
         width: 150px;
-      }
-    }
-  }
 
-  .passport-info {
-    font-size: $base-font-size;
-    width: 66%;
-
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-
-    .greeting {
-      background-color: $cream;
-      border-radius: 14px;
-      margin: 0 0 10px;
-      padding: 12px;
-    }
-    ul {
-      list-style: none;
-      width: 300px;
-
-      li {
-        padding: 8px 0 8px 5px;
-        border-bottom: 3px solid $cream;
-
-        &.island {
-          font-size: 0.7em;
+        &[src=""] {
+          visibility: hidden;
         }
-        &.title {
-          font-size: 0.8em;
+
+        @include phone-landscape {
+          height: 200px;
+          width: 200px;
         }
-        &.name {
-          font-size: 1em;
-        }
-        &.urls {
-          font-size: 0.7em;
+        
+        @include desktop {
+          height: 250px;
+          width: 250px;
         }
       }
     }
   }
+}
+
+.passport-info {
+  @include relative-in-place;
+  justify-self: stretch;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-columns: 1fr;
+  grid-template-rows: auto;
+  grid-auto-rows: auto;
+  font-size: 1.2rem;
+  
+  @include tablet-landscape {
+    width: 500px;
+  }
+
+  .greeting {
+    color: $olive-haze;
+    justify-self: flex-start;
+    background-color: $ecru-white;
+    border-radius: 14px;
+    margin: 0 0 10px;
+    padding: 12px 20px;
+
+    font-size: 1.1rem;
+    font-weight: 600;
+  }
+
+  ul {
+    list-style: none;
+    font-weight: 600;
+
+    li {
+      padding: 15px 0 15px 5px;
+      border-bottom: 3px solid $ecru-white;
+
+      a,
+      span,
+      .icon-contact-card {
+        vertical-align: middle;
+      }
+
+      &:nth-last-child(1) {
+        border-bottom: 0px solid $white;
+      }
+
+      &.island {
+        font-size: 0.9em;
+        color: $olive-haze;
+      }
+      &.title {
+        font-size: 0.9em;
+        color: $jambalaya;
+      }
+      &.name {
+        font-size: 0.9em;
+        color: $jambalaya;
+      }
+      &.urls {
+        font-size: 0.85em;
+        color: $olive-haze;
+      }
+    }
+  }
+}
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,112 +1,176 @@
 <template>
-   <div id='passports'>
-      <!-- todo add banner -->
-      <!-- todo add navigation -->
+  <div class="passports">
+    <!-- todo add banner -->
+    <!-- todo add navigation -->
+    <div
+      class="passport-wrapper"
+      v-for="member in members"
+      :key="members.indexOf(member)"
+    >
       <h1>
-         <hr/>
-         PASSPORTS
-         <hr/>
+        <hr />
+        <span>PASSPORT</span>
+        <hr />
       </h1>
-      <Passport 
-         v-for='member in members'
-         :key='members.indexOf(member)'
-         :member='member'
-      />
-   </div>
+      <Passport :member="member" />
+    </div>
+  </div>
 </template>
 
 <script>
-import Passport from '/components/partials/Passport.vue';
+import Passport from "/components/partials/Passport.vue";
 
 export default {
   components: {
-     Passport,
+    Passport,
   },
-  data: function() {
-     return {
+  data: function () {
+    return {
       members: [
-         {
-            name: 'Thulinma',
-            greeting: 'acpatterns.com!',
-            title: 'Computer Magician',
-            island: 'Thulopia',
-            urls: [
-               {
-                  name: 'Personal Site',
-                  url: 'https://thulinma.com',
-               },
-            ],
-         },
-         {
-            name: 'DamSenViet',
-            greeting: 'Sleeping atm.',
-            title: 'Sleeping Human',
-            island: 'Norende',
-            urls: [
-               {
-                  name: 'Personal Site',
-                  url: 'https://vietdaitran.com',
-               },
-            ],
-            roles: [
-               'Tool Developer',
-            ]
-         },
-         {
-            name: 'Myumi K.',
-            greeting: '(๑•̀ㅂ•́)و',
-            title: 'Aggressive Birthday Girl',
-            island: 'Willowtale',
-            urls: [
-               {
-                  name: 'GitHub',
-                  url: 'https://github.com/myumi',
-               },
-            ],
-            roles: [
-               'Tool Developer',
-            ]
-         },
-         {
-            name: 'Tero',
-            greeting: 'Graphic design is my passion',
-            title: 'Easy-Breezy Designer',
-            island: 'Koron',
-            urls: [
-               {
-                  name: 'Personal Site',
-                  url: 'https://tero.space',
-               },
-            ],
-         }
+        {
+          name: "Thulinma",
+          greeting: "acpatterns.com!",
+          title: "Computer Magician",
+          island: "Thulopia",
+          role: "",
+          urls: [
+            {
+              name: "Personal Site",
+              url: "https://thulinma.com",
+            },
+          ],
+        },
+        {
+          name: "DamSenViet",
+          role: "",
+          greeting: "Sleeping atm.",
+          title: "Sleeping Human",
+          island: "Norende",
+          urls: [
+            {
+              name: "Personal Site",
+              url: "https://vietdaitran.com",
+            },
+          ],
+          roles: ["Tool Developer"],
+        },
+        {
+          name: "Myumi K.",
+          role: "",
+          greeting: "(๑•̀ㅂ•́)و",
+          title: "Aggressive Birthday Girl",
+          island: "Willowtale",
+          urls: [
+            {
+              name: "GitHub",
+              url: "https://github.com/myumi",
+            },
+          ],
+          roles: ["Tool Developer"],
+        },
+        {
+          name: "Tero",
+          role: "",
+          greeting: "Graphic design is my passion",
+          title: "Easy-Breezy Designer",
+          island: "Koron",
+          urls: [
+            {
+              name: "Personal Site",
+              url: "https://tero.space",
+            },
+          ],
+        },
+        {
+          name: "MelonSpeedRuns",
+          role: "",
+          greeting: "Yahoo!!",
+          title: "Fruit Person",
+          island: "Fruita",
+          urls: [
+            {
+              name: "Twitter",
+              url: "https://twitter.com/MelonSpeedruns",
+            },
+          ],
+        },
       ],
-     };
-  }, 
-}
+    };
+  },
+};
 </script>
 
 <style lang='scss' scoped>
-$brown: #AE9E85;
-   #passports {
-      padding: 10px 30px;
+@import "styles/colors";
+@import "styles/screens";
 
-      h1 {
-         color: $brown;
-         font-size: 18px;
-         margin: 0 0 15px;
-         text-transform: uppercase;
+.passports {
+  padding: 50px 30px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-columns: 1fr;
+  grid-auto-rows: auto;
+  grid-template-rows: auto;
+  justify-content: center;
+  justify-items: center;
+  align-content: flex-start;
+  align-items: flex-start;
+  row-gap: 50px;
 
-         display: flex;
-         align-items: center;
-         justify-content: center;
+  h1 {
+    justify-self: stretch;
+    color: $donkey-brown;
+    font-size: 1.25rem;
+    text-transform: uppercase;
+    margin: 0 0 20px;
 
-         hr {
-            border-top: solid 2px $brown;
-            border-radius: 8px;
-            display: inline-block;
-            width: 100px;
-            margin: 0 30px;
-         }
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    grid-auto-columns: 1fr auto 1fr;
+    grid-auto-rows: auto;
+    grid-template-rows: auto;
+    justify-content: center;
+    justify-items: center;
+    align-content: flex-start;
+    align-items: center;
+    column-gap: 10px;
+
+    @include tablet-portrait {
+      margin: 0 0 40px;
+      column-gap: 30px;
+      grid-template-columns: auto auto auto;
+      grid-auto-columns: auto auto auto;
+    }
+
+    hr {
+      justify-self: stretch;
+      display: inline-block;
+      border-radius: 2px;
+
+      box-sizing: border-box;
+      width: calc(100% - 20px);
+      height: 3px;
+      background-color: $bison-hide;
+
+      @include tablet-portrait {
+        width: 150px;
+        height: 4px;
       }
-   }
+    }
+  }
+}
+
+.passport-wrapper {
+  justify-self: stretch;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-columns: 1fr;
+  grid-auto-rows: auto;
+  grid-template-rows: auto;
+  justify-content: center;
+  justify-items: center;
+  align-content: flex-start;
+  align-items: flex-start;
+}
 </style>
