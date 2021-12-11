569dda256774c7f46a6fea248c530c76a572ffc9
DamSenViet <26016342+DamSenViet@users.noreply.github.com>
2021-12-11 08:06:09 -0800

    Replaced Preview components

--- src/components/modals/Preview.vue
+++ src/components/modals/Preview.vue
@@ -1,38 +1,38 @@
 <template>
-  <VCard elevation="0" class="preview--card">
-    <div class="editor--qr-preview-window">
-      <img class="editor--qr-preview" :src="dataURL" alt="QR Code" />
-      <div class="editor--qr-buttons-container">
-        <button @click="downloadPNG" class="editor--qr-save-button">
-          Save QR
-        </button>
-        <button
-          v-if="drawingTool.compatMode === 'ACNH'"
-          @click="gameModeInfo = true"
-          class="editor--qr-info-button"
-        >
-          No QR Code?
-        </button>
-      </div>
-      
-      <VDialog
-        v-model="gameModeInfo"
-        content-class="qr-preview--dialog"
-        width="auto"
+  <VCard elevation="0" class="card">
+    <img class="preview" :src="dataURL" alt="QR Code" />
+    <div class="editor--qr-buttons-container">
+      <VBtn
+        class="save-btn rounded-lg"
+        @click="downloadPNG"
+        elevation="0"
       >
-        <Info v-if="gameModeInfo" @close="gameModeInfo = false">
-          <ACNLToACNHInfo />
-        </Info>
-      </VDialog>
+        Save QR
+      </VBtn>
+      <VBtn
+        v-if="drawingTool.compatMode === 'ACNH'"
+        class="qr-info-btn rounded-lg"
+        @click="gameModeInfo = true"
+        elevation="0"
+      >
+        No QR Code?
+      </VBtn>
     </div>
+
+    <VDialog
+      v-model="gameModeInfo"
+      content-class="qr-preview--dialog"
+      width="auto"
+    >
+      <Info v-if="gameModeInfo" @close="gameModeInfo = false">
+        <ACNLToACNHInfo />
+      </Info>
+    </VDialog>
   </VCard>
 </template>
 
 <script>
-import {
-  VDialog,
-  VCard,
-} from "vuetify/lib";
+import { VDialog, VBtn, VCard } from "vuetify/lib";
 import DrawingTool from "~/libs/DrawingTool";
 import Info from "~/components/modals/Info.vue";
 import ACNLToACNHInfo from "~/components/partials/ACNLToACNHInfo.vue";
@@ -43,17 +43,14 @@ import ACNLQRGenerator from "~/components/ACNLQRGenerator.vue";
 import generateACNLQR from "~/libs/ACNLQRGenerator";
 import generateACNHPBL from "~/libs/ACNHPBLGenerator";
 
-/* svg icons */
-import IconBase from "~/components/icons/IconBase.vue";
-import IconQRCode from "~/components/icons/IconQRCode.vue";
+import colors from "~/styles/colors.scss";
 
 export default {
   name: "Preview",
   components: {
     VDialog,
+    VBtn,
     VCard,
-    IconBase,
-    IconQRCode,
     ACNLQRGenerator,
     Info,
     ACNLToACNHInfo,
@@ -67,6 +64,7 @@ export default {
   data: function () {
     const dataURL = "";
     return {
+      colors,
       dataURL,
       gameModeInfo: false,
     };
@@ -88,36 +86,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use "styles/overrides" as overrides;
 @import "styles/colors";
 @import "styles/positioning";
 @import "styles/screens";
 @import "styles/resets";
 
-.preview--card {
+.card {
   background-color: transparent;
-  position: relative;
+  @include relative-in-place;
 }
 
-.editor--qr-preview-window {
-  max-width: 100%;
-  z-index: 999;
-}
-
-.editor--qr-preview {
+.preview {
   display: block;
-  border-width: 5px;
+  border-width: 2px;
   border-style: solid;
   border-color: $cannon-pink;
   background-color: $cannon-pink;
-  border-radius: 30px;
-
-  @include tablet-landscape {
-    width: auto;
+  border-radius: 30px !important;
+  max-width: 100%;
+  max-height: 500px;
+  @include phone-landscape {
+    border-width: 5px;
   }
 }
 
 .editor--qr-buttons-container {
-  pointer-events: none;
   margin-top: 30px;
   display: grid;
   justify-content: center;
@@ -127,33 +121,18 @@ export default {
   row-gap: 10px;
 }
 
-.editor--qr-save-button,
-.editor--qr-info-button {
-  @include reset-button;
-  cursor: pointer;
-
-  border-width: 4px;
-  border-style: solid;
-  border-color: $tiffany-blue;
-  background: $tiffany-blue;
-  pointer-events: auto;
-  border-radius: 8px;
-  font-weight: 600;
+.save-btn,
+.qr-info-btn {
+  @include overrides.v-btn($white, $robin-egg-blue);
   font-size: 1.1rem;
-  padding: 5px 30px;
-  color: white;
-
+  border: 4px solid $robin-egg-blue;
   &:hover {
-    border-color: $turquoise;
-    @include stripes($tiffany-blue, $tiffany-blue-light, 15px);
-    @include moving-stripes(3s);
+    @include stripes($tiffany-blue, $tiffany-blue-light, 20px);
+    @include moving-stripes(8s);
+    border: 4px solid $turquoise;
   }
 }
 
-.cancel-button-adjust {
-  top: -5px;
-  right: -5px;
-}
 </style>
 
 <style lang="scss">
