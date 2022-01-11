dd59f21d90601a0bf3a4426d66e36292bb79588f
DamSenViet <26016342+DamSenViet@users.noreply.github.com>
2022-01-10 19:40:02 -0800

    Converted i18n file to ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,19 +1,26 @@
 import Vue from 'vue';
-import VueI18n from 'vue-i18n';
+import VueI18n, { LocaleMessages } from 'vue-i18n';
 import Papa from "papaparse";
 import { isProd } from '@/utils/if-env';
 import translationsCsvString from "./translations.csv";
 
 Vue.use(VueI18n);
 
-const translations = Papa.parse(translationsCsvString, {
-  dynamicTyping: false,
-  header: true,
-  skipEmptyLines: true,
-}).data;
+interface Translation {
+  path: string,
+  [key: string]: string,
+}
+
+const translations = Papa.parse<Translation>(
+  translationsCsvString,
+  {
+    dynamicTyping: false,
+    header: true,
+    skipEmptyLines: true,
+  },
+).data;
 
 const messages = translations.reduce((messages, row) => {
-  /**@type {string} */
   const path = row["path"];
   const pathComponents = path.split(".");
   // locales are ISO 639-1 codes
@@ -23,7 +30,7 @@ const messages = translations.reduce((messages, row) => {
   // traverse messages and create properties
   for (const locale of locales) {
     const message = row[locale];
-    if (message.trim().length <= 0 ) continue;
+    if (message.trim().length <= 0) continue;
     const localePathComponents = [locale, ...pathComponents];
     let messagesNode = messages;
     for (const pathComponent of localePathComponents.slice(0, -1)) {
@@ -34,7 +41,7 @@ const messages = translations.reduce((messages, row) => {
     messagesNode[localePathComponents.slice(-1).pop()] = message;
   }
   return messages;
-}, {});
+}, {}) as LocaleMessages;
 
 const i18n = new VueI18n({
   locale: navigator.language.toLowerCase(), // use default and fallback languages
