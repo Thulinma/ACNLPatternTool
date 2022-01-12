059f0437b5cdc0086d9013ee729938ce42ee5b0b
DamSenViet <26016342+DamSenViet@users.noreply.github.com>
2022-01-11 23:07:56 -0800

    Added shims for remaining loader configs

--- src/shims-loaders.d.ts
+++ src/shims-loaders.d.ts
@@ -1,4 +1,36 @@
+declare module "*.module.css";
+
+declare module "*.scss" {
+  const value: Record<string, string>;
+  export default value;
+}
+
+declare module "*.sass" {
+  const value: Record<string, string>;
+  export default value;
+}
+
+declare module "*.md" {
+  import Vue from "vue"
+  export default Vue;
+}
+
 declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
   const value: string;
   export default value;
 }
