516428a597dae1ea9b46aa0a1caa0b8596fc3246
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-01-07 23:43:38 -0800

    Converted Storage component to ts

--- src/pages/Editor/Storage.vue
+++ src/pages/Editor/Storage.vue
@@ -10,14 +10,13 @@
   </PatternContainer>
 </template>
 
-<script>
+<script lang="ts">
 import { first, without } from "lodash";
-import {
-  mapGetters,
-  mapMutations,
-  mapActions,
-} from "vuex";
+import { namespace } from "vuex-class";
+import { Vue, Component } from "vue-property-decorator";
 import PatternContainer from "@/components/positioned/PatternContainer.vue";
+import { UtilityBarOption } from "@/components/positioned/UtilityBar.vue"
+import { PatternItem } from "@/libs/storage";
 import {
   drawingToolToNamedPatternBlob,
   drawingToolToNamedImageBlob,
@@ -25,164 +24,168 @@ import {
   downloadNamedBlob,
 } from "@/libs/downloader";
 
-export default {
+const storageModule = namespace('storage');
+
+@Component({
   name: "Storage",
   components: {
     PatternContainer,
   },
-  data() {
-    return {
-      selected: [],
+})
+export default class Storage extends Vue {
+  selected: PatternItem[] = [];
+  
+  @storageModule.Getter('patternItems') readonly patternItems!: PatternItem[];
+  
+  get drawingTools() {
+    return this.patternItems.map(pi => pi.drawingTool);
+  }
+  
+  get options() {
+    const { selected, patternItems } = this;
+    let options: UtilityBarOption[] = [];
+    if (patternItems.length === 0) return options;
+
+    const open = {
+      icon: 'mdi-application-edit',
+      label: `Open`,
+      callback: async () => {
+        const patternItem = first(selected) as PatternItem;
+        this.$emit("load", [patternItem.drawingTool]);
+        this.$emit("close");
+      },
     };
-  },
-  computed: {
-    ...mapGetters("storage", ["patternItems"]),
-    drawingTools() {
-      return this.patternItems.map(pi => pi.drawingTool);
-    },
-    options() {
-      const { selected, patternItems } = this;
-      let options = [];
-      if (patternItems.length === 0) return options;
 
-      const open = {
-        icon: 'mdi-application-edit',
-        label: `Open`,
-        callback: async () => {
-          const patternItem = first(selected);
-          this.$emit("load", [patternItem.drawingTool]);
-          this.$emit("close");
-        },
-      };
+    const del = {
+      icon: 'mdi-trash-can',
+      label: `Delete`,
+      callback: async () => {
+        const [
+          message,
+          source
+        ] = selected.length !== 0
+          ? [
+            "Are you sure you want to delete these patterns from storage?",
+            selected,
+          ]
+          : [
+            "Are you sure you want to clear the storage?",
+            patternItems,
+          ];
+        if (!window.confirm(message))
+          return;
+        await this.remove(source);
+        this.selected = without(
+          this.selected,
+          ...source,
+        );
+      },
+    };
 
-      const del = {
-        icon: 'mdi-trash-can',
-        label: `Delete`,
-        callback: async () => {
-          const [
-            message,
-            source
-          ] = selected.length !== 0
-            ? [
-              "Are you sure you want to delete these patterns from storage?",
-              selected,
-            ]
-            : [
-              "Are you sure you want to clear the storage?",
-              patternItems,
-            ];
-          if (!window.confirm(message))
-            return;
-          await this.remove(source);
-          this.selected = without(
-            this.selected,
-            ...source,
+    const downloadAsPattern = {
+      icon: 'mdi-file',
+      label: `.ACNL/.ACNH`,
+      callback: async () => {
+        if (selected.length === 1) {
+          const namedPatternBlob = await drawingToolToNamedPatternBlob(
+            (first(selected) as PatternItem).drawingTool
           );
-        },
-      };
+          await downloadNamedBlob(namedPatternBlob);
+          return;
+        }
+        const source = selected.length
+          ? selected
+          : patternItems;
+        const namedPatternBlobs = await Promise.all(
+          source
+            .map(pi => pi.drawingTool)
+            .map(dt => drawingToolToNamedPatternBlob(dt))
+        );
+        const namedZipBlob = await namedBlobsToNamedZipBlob(namedPatternBlobs);
+        await downloadNamedBlob(namedZipBlob);
+      },
+    };
 
-      const downloadAsPattern = {
-        icon: 'mdi-file',
-        label: `.ACNL/.ACNH`,
-        callback: async () => {
-          if (selected.length === 1) {
-            const namedPatternBlob = await drawingToolToNamedPatternBlob(
-              first(selected).drawingTool
-            );
-            await downloadNamedBlob(namedPatternBlob);
-            return;
-          }
-          const source = selected.length
-            ? selected
-            : patternItems;
-          const namedPatternBlobs = await Promise.all(
-            source
-              .map(pi => pi.drawingTool)
-              .map(dt => drawingToolToNamedPatternBlob(dt))
+    const downloadAsPng = {
+      icon: 'mdi-image',
+      label: `QR/PBL`,
+      callback: async () => {
+        if (selected.length === 1) {
+          const namedImageBlob = await drawingToolToNamedImageBlob(
+            (first(selected) as PatternItem).drawingTool
           );
-          const namedZipBlob = await namedBlobsToNamedZipBlob(namedPatternBlobs);
-          await downloadNamedBlob(namedZipBlob);
-        },
-      };
+          await downloadNamedBlob(namedImageBlob);
+          return;
+        }
+        const source = selected.length
+          ? selected
+          : patternItems;
+        const namedImageBlobs = await Promise.all(
+          source
+            .map(pi => pi.drawingTool)
+            .map(dt => drawingToolToNamedImageBlob(dt))
+        );
+        const namedZipBlob = await namedBlobsToNamedZipBlob(namedImageBlobs);
+        await downloadNamedBlob(namedZipBlob);
+      },
+    };
 
-      const downloadAsPng = {
-        icon: 'mdi-image',
-        label: `QR/PBL`,
-        callback: async () => {
-          if (selected.length === 1) {
-            const namedImageBlob = await drawingToolToNamedImageBlob(
-              first(selected).drawingTool
-            );
-            await downloadNamedBlob(namedImageBlob);
-            return;
-          }
-          const source = selected.length
-            ? selected
-            : patternItems;
-          const namedImageBlobs = await Promise.all(
-            source
-              .map(pi => pi.drawingTool)
-              .map(dt => drawingToolToNamedImageBlob(dt))
+    const downloadAsBoth = {
+      icon: 'mdi-zip-box',
+      label: `Both`,
+      callback: async () => {
+        if (selected.length === 1) {
+          const namedPatternBlob = await drawingToolToNamedPatternBlob(
+            (first(selected) as PatternItem).drawingTool
           );
-          const namedZipBlob = await namedBlobsToNamedZipBlob(namedImageBlobs);
-          await downloadNamedBlob(namedZipBlob);
-        },
-      };
-
-      const downloadAsBoth = {
-        icon: 'mdi-zip-box',
-        label: `Both`,
-        callback: async () => {
-          if (selected.length === 1) {
-            const namedPatternBlob = await drawingToolToNamedPatternBlob(
-              first(selected).drawingTool
-            );
-            const namedImageBlob = await drawingToolToNamedImageBlob(
-              first(selected).drawingTool
-            );
-            const namedZipBlob = await namedBlobsToNamedZipBlob([
-              namedPatternBlob,
-              namedImageBlob,
-            ]);
-            await downloadNamedBlob(namedZipBlob);
-            return;
-          }
-          let source = selected.length
-            ? selected
-            : patternItems;
-          const namedPatternBlobs = await Promise.all(
-            source
-              .map(pi => pi.drawingTool)
-              .map(dt => drawingToolToNamedPatternBlob(dt))
-          );
-          const namedImageBlobs = await Promise.all(
-            source
-              .map(pi => pi.drawingTool)
-              .map(dt => drawingToolToNamedImageBlob(dt))
+          const namedImageBlob = await drawingToolToNamedImageBlob(
+            (first(selected) as PatternItem).drawingTool
           );
           const namedZipBlob = await namedBlobsToNamedZipBlob([
-            ...namedPatternBlobs,
-            ...namedImageBlobs,
+            namedPatternBlob,
+            namedImageBlob,
           ]);
           await downloadNamedBlob(namedZipBlob);
-        },
-      };
+          return;
+        }
+        let source = selected.length
+          ? selected
+          : patternItems;
+        const namedPatternBlobs = await Promise.all(
+          source
+            .map(pi => pi.drawingTool)
+            .map(dt => drawingToolToNamedPatternBlob(dt))
+        );
+        const namedImageBlobs = await Promise.all(
+          source
+            .map(pi => pi.drawingTool)
+            .map(dt => drawingToolToNamedImageBlob(dt))
+        );
+        const namedZipBlob = await namedBlobsToNamedZipBlob([
+          ...namedPatternBlobs,
+          ...namedImageBlobs,
+        ]);
+        await downloadNamedBlob(namedZipBlob);
+      },
+    };
 
-      if (selected.length === 1)
-        options.push(open);
-      options.push(del);
-      options.push(
-        downloadAsPattern,
-        downloadAsPng,
-        downloadAsBoth,
-      );
-      return options;
-    },
-  },
-  methods: {
-    ...mapMutations("storage", ["init"]),
-    ...mapActions("storage", ["remove"]),
-  },
+    if (selected.length === 1)
+      options.push(open);
+    options.push(del);
+    options.push(
+      downloadAsPattern,
+      downloadAsPng,
+      downloadAsBoth,
+    );
+    return options;
+  }
+  
+  @storageModule.Mutation('init')
+  init!: () => Promise<void>;
+  
+  @storageModule.Action('remove')
+  remove!: (patternItems: PatternItem[]) => Promise<void>
+  
   mounted() {
     this.init();
   }
